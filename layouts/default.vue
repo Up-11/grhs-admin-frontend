6e7de3cac08bde5ae79fb8366fced404c7637e12
up-11 <iurykolgin@gmail.com>
2025-03-30 16:44:25 +0700

    test: lazy git

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="h-screen">
+		<aside>1</aside>
+		<header>2</header>
+		<main>
+			<slot />
+		</main>
+	</div>
+</template>
+
+<style scoped></style>
