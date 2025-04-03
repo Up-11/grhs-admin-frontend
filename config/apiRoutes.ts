5420270632262affb405ab5a688bd820acdb2470
up-11 <iurykolgin@gmail.com>
2025-04-04 01:27:23 +0700

    fix: routes

--- config/apiRoutes.ts
+++ config/apiRoutes.ts
@@ -1,6 +1,6 @@
 export const API_ROUTES = {
-	HOST: 'https://api.up-development.ru',
-	BASE_URL: 'https://api.up-development.ru/api',
+	HOST: 'https://api.up-mailer.ru',
+	BASE_URL: 'https://api.up-mailer.ru/api',
 	GET_ALL_ADMINISTRATORS: '/administrators',
 	MEDIA: '/media',
 	AUTH: {
